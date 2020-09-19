84e1dace4a46c630200f921834cd42286c25bfb1
Alexande Becker <becker.alexander42@gmail.com>
2020-09-19 22:49:40 +0200

    added the start command to the program and implemented the file write part

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use moessbauer_filter::{
     MBFError,
     MBFState,
 };
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 use std::process::exit;
-use tokio::net::{TCPListener, TcpStream};
 
 fn main() {
     let matches = App::new("Moessbauer Filter")
@@ -73,13 +75,18 @@ fn main() {
                 .help("file path where the results of the measurement are written to CAUTION: Be aware of disk space")
                 .takes_value(true)
                 .index(1)
+                .required(true))
+            .arg(Arg::with_name("target file size")
+                .short("s")
+                .long("target-file-size")
+                .help("The file size that should be collected before the measurement is automatically stopped")
+                .takes_value(true)
                 .required(true)))
-        .subcommand(SubCommand::with_name("stop")
-            .about("command that stops the measurement. If the filter is not running the command has no effect"))
         .subcommand(SubCommand::with_name("status")
             .about("command that returns the current state of the hardware filter with the currently loaded configuration"))
         .get_matches();
 
+    // configure subcommand
     if let Some(matches) = matches.subcommand_matches("configure") {
         if let Ok(filter) = MBFilter::new() {
             let config = MBConfig::new_from_str(matches.value_of("k").unwrap(),
@@ -102,13 +109,71 @@ fn main() {
             println!("The memmory map failed, there may be another instance of this program running");
         }
     }
+
+    // start subcommand
     if let Some(matches) = matches.subcommand_matches("start") {
-        println!("start subcommand");
+        if let Ok(mut filter) = MBFilter::new() {
+            let requested_pc = match u64::from_str_radix(matches.value_of("peakcount").unwrap(), 10) {
+                Ok(val) => val,
+                Err(_) => {
+                    println!("the value for the peakcount has to be a natural number");
+                    exit(1)
+                },
+            };
+            if let Some(filepath) = matches.value_of("output file") {
+                let path = Path::new(filepath);
+                let mut ofile = match File::create(&path) {
+                    Err(why) => panic!("could not create {}: {}", path.display(), why),
+                    Ok(file) => file,
+                };
+                let mut fc: u64 = 0;
+                match filter.state() {
+                    MBFState::Ready => {
+                        filter.start();
+                        let mut buffer: [u8; 12*2048] = [0; 12*2048];
+                        while fc < requested_pc {
+                            let bytes_read = match filter.read(&mut buffer) {
+                                Ok(val) => val,
+                                Err(e) => {
+                                    panic!("Error encountered: {}",e);
+                                },
+                            };
+                            let mut pos = 0;
+                            while pos < (&buffer[..bytes_read]).len() {
+                                let bytes_written = ofile.write(&buffer[pos..]).unwrap();
+                                pos += bytes_written;
+                            }
+                            match ofile.write(&buffer[..bytes_read]) {
+                                Ok(val) => {
+                                    if val != bytes_read {
+                                        panic!("Could not write all the to the file");
+                                    }
+                                },
+                                Err(e) => {
+                                    panic!("The Error {} occured while writing to the file", e);
+                                }
+                            };
+                            fc += bytes_read as u64;
+                        }
+                    },
+                    _ => {
+                        println!("The filter is in wrong state please check the state via the state subcommand");
+                        exit(1);
+                    }
+                };
+            }
+        } else {
+            println!("The memmory map failed, there may be another instance of this program running");
+        }
     }
-    if let Some(matches) = matches.subcommand_matches("stop") {
+
+    // stop subcommand
+    if let Some(_) = matches.subcommand_matches("stop") {
         println!("stop subcommand");
     }
-    if let Some(matches) = matches.subcommand_matches("status") {
+
+    // status subcommand
+    if let Some(_) = matches.subcommand_matches("status") {
         if let Ok(filter) = MBFilter::new() {
             let config = filter.configuration();
             let state = filter.state();
